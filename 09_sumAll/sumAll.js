41ab81c797c49c7ff1787ced40d9b8354d39bdfb
SshauryaaRocks19 <198586011+SshauryaaRocks19@users.noreply.github.com>
2025-10-17 19:15:07 +0530

    solve

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let arr = [];
+
+    if(a<0 || b<0 || isNaN(a) || isNaN(b) || !Number.isInteger(a)|| !Number.isInteger(b) ){
+        return "ERROR";
+    }
+
+
+    if (a>b){
+        [b, a] = [a, b];
+    }
+
+    for(i=a;i<=b;i++){
+        arr.push(i);
+    }
+
+    let sum = arr.reduce((accumulator, item)=>{
+        return accumulator+item;
+    },0)
+    return sum;
 
 };
 
